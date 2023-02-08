c27bbb6d5f77d95fc7ad9821ee0d84c513c1a43a
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-08 11:06:09 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import cavil from './henry-cavill.jpg'
 
 export default function Navbar() {
   return (
-    <div className='bg-gray-800 p-5'>
-        
+    <div className='bg-gray-800 p-2'>
+        <img src={cavil} alt="" width="100" height="100"></img>
     </div>
   )
 }
