0cc2cfc650eacd9c4fa62ddffa6cc28c0ec4293b
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-08 15:42:31 +0530

    Added product cards

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,16 @@ import cavil from './henry-cavill.jpg'
 
 export default function Navbar() {
   return (
-    <div className='bg-gray-800 p-2'>
-        <img src={cavil} alt="" width="100" height="100"></img>
+    <div className='bg-gray-800 p-2 flex justify-between'>
+        <img className="rounded-[50%]" src={cavil} alt="" width="100px" height="100px"></img>
+        <div>
+        <input type="text" />
+        </div>
+        
+        <label className="text-white" htmlFor="cart">1</label>
+        <img name="cart" 
+        src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/null/external-shopping-cart-miscellaneous-kiranshastry-lineal-kiranshastry.png"
+        alt=""/>
     </div>
   )
 }
