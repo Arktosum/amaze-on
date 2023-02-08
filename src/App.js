a6a7bfab13f2526d639a0fdf6b8a455a448aacc6
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-08 10:31:17 +0530

    Initialize files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Content from './Components/Content';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (<>
+  <Navbar/>
+  <Content/>
+  </>);
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='bg-gray-800 p-5'>
+        <img src= "./cat.png"></img>
+    </div>
+  )
+}
