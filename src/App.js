252aacfbf06c7712bc4f32b052f5b4f0aa888680
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-09 09:53:04 +0530

    Added Search and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import './App.css';
-import Content from './Components/Content';
 import Navbar from './Components/Navbar';
+import ProductPage from './Components/ProductPage';
+import { Route,Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 function App() {
-  return (<>
+
+  return (
+  <>
   <Navbar/>
-  <Content/>
-  </>);
+  <Routes>
+    <Route path="/" element={<ProductPage/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+  </Routes>
+  </>
+
+  );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import cavil from './henry-cavill.jpg'
 
 export default function Navbar() {
+
   return (
     <div className='bg-gray-800 p-2 flex justify-between'>
-        <img className="rounded-[50%]" src={cavil} alt="" width="100px" height="100px"></img>
-        <div>
-        <input type="text" />
-        </div>
-        
-        <label className="text-white" htmlFor="cart">1</label>
+        <img src={cavil} alt="" className='rounded-full w-20 h-20'/>
+        <p className='text-white'>1</p>
         <img name="cart" 
         src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/null/external-shopping-cart-miscellaneous-kiranshastry-lineal-kiranshastry.png"
-        alt=""/>
+        alt=""
+        className='w-20 h-20'/>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+            <li>
+              <Link to="signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
     </div>
   )
 }
