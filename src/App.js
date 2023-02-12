a1546d2dd8277afe8a8dbebe606270422b983d78
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-12 23:02:05 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import { Route,Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import ProductPage from './Components/ProductPage';
 import CheckoutPage from './Components/CheckoutPage';
+import React from 'react'
 
-function App() {
-
+export default function App() {
+  let [userCart,addToCart] = React.useState([])
+  
   return (
   <>
   <Navbar/>
   <Routes>
-    <Route path="/" element={<ProductPage/>}></Route>
-    <Route path="/checkout" element={<CheckoutPage/>}></Route>
+    <Route path="/" element={<ProductPage addToCart={addToCart} />}></Route>
+    <Route path="/checkout" element={<CheckoutPage cart={userCart} />}></Route>
   </Routes>
   </>
-
   );
 }
 
-export default App;
--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -10,6 +10,7 @@ let products =  {
 }
 function ProductCard(props){
   let item = props.props
+  let addToCart = props.addToCart
   return(<>
     <div className="bg-gradient-to-tl from-green-400 to-purple-500 product w-[250px] h-[360px] hover:scale-105 duration-200 cursor-pointer ease-in-out hover:shadow-sm hover:shadow-white rounded-xl">
       <div className="h-[290px]">
@@ -19,15 +20,20 @@ function ProductCard(props){
         <div className='text-white overflow-hidden max-h-[28px]'>Name : {item['name']}</div>
         <div className='text-white text-bold overflow-hidden max-h-[30px]'>Price : {'₹ ' + item['price']}</div>
       </div>
+      <div className='h-10 w-10 bg-red-500 absolute top-5 left-3/4 z-0 rounded-full' onClick={(e)=>{
+        alert("Added item : " + item['name'])
+        addToCart(prev=>[...prev,item['id']])
+      }}></div>
     </div>
   </>)
 }
-export default function ProductPage() {
+export default function ProductPage(props) {
   let [formData,setformData] = useState({searchString:"",order:'asc'})
-  console.log(formData)
+  let addToCart = props.addToCart
+
+
   let regex = new RegExp(formData.searchString,'i')
 
-  
   let ele = []
   for(let ID in products) {
     products[ID]['id'] = ID
@@ -45,7 +51,7 @@ export default function ProductPage() {
     return (a.price - b.price)
   })
   let productElements = ele.map((item)=>{
-    return <ProductCard key={item['id']}props={item}/>
+    return <ProductCard key={item['id']}props={item} addToCart={addToCart}/>
   })
   
 
