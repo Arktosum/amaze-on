e56a65f22266f1b17b393416af113fb1f6697e89
praneash <praneash2@gmail.com>
2023-02-15 00:35:04 +0530

    about us page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import React from 'react'
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import Contact from './Components/Contact';
-
+import About from './Components/About';
 export default function App() {
   let [userCart,addToCart] = React.useState([])
   
@@ -22,6 +22,7 @@ export default function App() {
     <Route path="/product" element={<ProductPage addToCart={addToCart} cart={userCart}/>}></Route>
     <Route path="/checkout" element={<CheckoutPage cart={userCart}/>}></Route>
     <Route path="/contact" element={<Contact/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
   </Routes>
   </ChakraProvider>
   </>
