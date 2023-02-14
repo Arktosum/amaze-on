c7803134a7aafc042aabfa714a611d517052fc1f
praneash <praneash2@gmail.com>
2023-02-14 23:54:32 +0530

    mior changes in contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -28,16 +28,16 @@ export default function Contact() {
   return (<>
     <div className="flex flex-col justify-center min-h-[100vh] w-[100%] h-[100%] m-0% items-center bg-[#000000]">
         <div>
-        <div ><h1 className='text-[white]'>You can post your queries here</h1></div>
+        <div ><h1 className='font-bold text-[21px]  text-[white]'>You can post your queries here</h1></div>
         
             
-        <div><label className='mt-[10px] text-[white] '>Enter email  </label></div>
+        <div className='mt-5 mb-2'><label className=' text-[white] '>Enter email  </label></div>
         <div><input className="bg-gray w-[250px] h-[40px] rounded-[5px] hover:border-2 border-[#F97707] focus:outline-none p-2.5 dark:bg-[#d9d9d9]" type="email" id="email" value={get_email} onChange={(e)=> set_email(e.target.value)}/></div>
 
-        <div> <label className='mt-5 text-[white] ' >Enter your query</label> </div>
+        <div className='mt-5 mb-2'> <label className='mt-5 text-[white] ' >Enter your query</label> </div>
         <div> <textarea className="bg-gray w-[250px]  rounded-[5px] hover:border-2 border-[#F97707] focus:outline-none p-2.5 dark:bg-[#d9d9d9]" type="text" id="query" value={input} onChange={(e)=> setinput(e.target.value)}/>    </div>
         
-        <div><input className='text-[white]' type="submit" onClick={add_data}/></div>
+        <div><input className="w-[250px] bg-[#f97316] hover:bg-[#fb923c] text-white font-bold py-2 px-4 rounded-[5px] mt-5 text-[white]" type="submit" onClick={add_data}/></div>
         <div>
         <ul>
             {list.map((input)=>(
