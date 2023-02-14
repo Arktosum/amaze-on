a6c36193010d7bad8b952285fa4bec2c50ba1b65
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-14 11:10:02 +0530

    Login Signup Cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
+import { Link, Route,Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import ProductPage from './Components/ProductPage';
 import CheckoutPage from './Components/CheckoutPage';
 import {ChakraProvider} from '@chakra-ui/react'
 import React from 'react'
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 export default function App() {
   let [userCart,addToCart] = React.useState([])
@@ -14,9 +16,14 @@ export default function App() {
   <>
   <ChakraProvider>
   <Navbar/>
+  <Link to='/login'>LOGIN</Link>
+  <Link to='/signup'>SIGNUP</Link>
   <Routes>
     <Route path="/" element={<ProductPage addToCart={addToCart} cart={userCart}/>}></Route>
-    <Route path="/checkout" element={<CheckoutPage cart={userCart} />}></Route>
+    <Route path="/checkout" element={<CheckoutPage cart={userCart}/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+
   </Routes>
   </ChakraProvider>
   </>
