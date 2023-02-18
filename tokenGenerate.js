88a1682686c09c2a6f2f0efc07349d4b98682ee4
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-02-18 13:42:16 +0530

    Cleaning

--- tokenGenerate.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-require("dotenv").config()
-
-const generateToken = (email)=>{
-    return jwt.sign({email}, process.env.SECRET_KEY)
-}
-
-module.exports = generateToken;
